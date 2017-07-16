5a80d4a6a83855921366b33d6cacbfd14dc0b0a8
Eric <peoplefromgoodhomes@gmail.com>
2017-07-16 10:51:18 -0400

    rm purge

--- src/store.js
+++ src/store.js
@@ -28,6 +28,6 @@ const enhancer = composeEnhancers(
 );
 const store = createStore(reducer, undefined, enhancer);
 sagaMiddleware.run(rootSaga)
-persistStore(store).purge()
+persistStore(store)
 
 export default store
