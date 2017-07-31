7a044957701a8b567f9f350ad78b1a81dace1664
Eric <peoplefromgoodhomes@gmail.com>
2017-07-30 23:25:30 -0400

    make Individual a singleton, no tests yet

--- contracts/Individual.sol
+++ contracts/Individual.sol
@@ -2,14 +2,46 @@ pragma solidity ^0.4.0;
 import "./zeppelin/lifecycle/Killable.sol";
 
 contract Individual is Killable {
-  string public name;
-  
-	function Individual(string _name) {
-		name = _name;
-		owner = msg.sender;
+	mapping (address => string) public name;
+	mapping (address => address[]) public schedules;
+	mapping (address => string) public image;
+
+	function getName(address addr) public returns (string) {
+		return name[addr];
 	}
 
-	function getName() public returns (string) {
-		return name;
+	function setName(string _name) public {
+		name[msg.sender] = _name;
+	}
+
+	function getSchedules() public returns (address[]) {
+		return schedules[msg.sender];
+	}
+
+	function addSchedule(address schedule) public {
+		schedules[msg.sender].push(schedule);
+	}
+
+	function removeSchedule(address schedule) public {
+		//find it
+		for(uint i = 0; i < schedules[msg.sender].length; i++) {
+			address found = schedules[msg.sender][i];
+			if (found == schedule) {
+				for (uint j = i; j < schedules[msg.sender].length - 1; j++) {
+					schedules[msg.sender][j] = schedules[msg.sender][j+1];
+				}
+				delete schedules[msg.sender][schedules[msg.sender].length - 1];
+				schedules[msg.sender].length--;
+				break;
+			}
+		}
+	}
+
+	function getImage(address addr) public returns (string) {
+		return image[addr];
+	}
+
+	function setImage(string url) public {
+		image[msg.sender] = url;
 	}
 }
