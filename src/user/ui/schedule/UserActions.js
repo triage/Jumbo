e8a2d71a7ef072ac0f7692e92b44538c3e9203d4
Eric <peoplefromgoodhomes@gmail.com>
2017-10-12 00:04:49 -0400

    wip reserve as reseller for user

--- src/user/ui/schedule/UserActions.js
+++ src/user/ui/schedule/UserActions.js
@@ -1,79 +1,115 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import UserType from 'src/user/model/UserType'
 import eth from 'src/util/eth'
-import { Field } from 'redux-form'
+import { reduxForm, Field } from 'redux-form'
 
-const UserActions = props => {
-  const {
-    user,
-    location,
-    history,
-    handleSubmit,
-    pristine,
-    submitting,
-    scheduleCancel,
-    scheduleComplete,
-    spotPurchase,
-    spotCancel,
-    schedule,
-  } = props;
+class UserActions extends PureComponent {
 
-  if (user.type === UserType.studio) {
-    const balance = eth.web3().fromWei(schedule.balance)
-    //studio can only cancel if current date is before class
-    if (new Date() < new Date(schedule.dates.start)) {
-      return (
-        <form
-          onSubmit={handleSubmit(values => {
-            scheduleCancel(schedule.address, values.reason, history)
-          })}
-        >
-          <div>Balance: {balance} eth</div>
-          <span>Cancel:</span>
-          <Field name="reason" component="input" type="text" placeholder="cancellation reason" />
-          <input disabled={pristine || submitting} type="submit" value="Cancel" />
-        </form>
-      )
-    } else {
-      //studio can complete contract
-      return (
-        <button onClick={event => {
-        console.log('clicked')
-        scheduleComplete(schedule.address, history)
-        }}>Complete class and withdraw ${balance}</button>
-      )
+  constructor() {
+    super()
+    this.state = {
+      name: null,
+      timer: null,
     }
-  } else if (user.type === UserType.individual || user.type === UserType.reseller) {
-    const price = user.type === UserType.individual ? schedule.price.individual : schedule.price.reseller
-    if (schedule.reserved) {
-      if (new Date().valueOf() < new Date(schedule.dates.cancellation).valueOf()) {
+  }
+
+  onAddressChanged(event) {
+    clearTimeout(this.state.timer)
+    this.setState({
+      inputValid: false,
+      name: null,
+      timer: setTimeout(() => {
+        eth.Individual().deployed().then(reseller => {
+          return reseller.getName.call(event.target.value)
+        }).then(name => {
+          this.setState({
+            inputValid: false,
+            name
+          })
+        })
+      }, 1000)
+    })
+  }
+
+  render() {
+    const {
+    user,
+      location,
+      history,
+      handleSubmit,
+      pristine,
+      submitting,
+      scheduleCancel,
+      scheduleComplete,
+      spotPurchase,
+      spotCancel,
+      schedule,
+  } = this.props;
+
+    if (user.type === UserType.studio) {
+      const balance = eth.web3().fromWei(schedule.balance)
+      //studio can only cancel if current date is before class
+      if (new Date() < new Date(schedule.dates.start)) {
         return (
-          <button type="button" onClick={event => spotCancel(schedule, user.address, history, location)}>
-            Cancel and refund
-          </button>
+          <form
+            onSubmit={handleSubmit(values => {
+              scheduleCancel(schedule.address, values.reason, history)
+            })}
+          >
+            <div>Balance: {balance} eth</div>
+            <span>Cancel:</span>
+            <Field name="reason" component="input" type="text" placeholder="cancellation reason" />
+            <input disabled={pristine || submitting} type="submit" value="Cancel" />
+          </form>
         )
       } else {
+        //studio can complete contract
         return (
-          <span>The cancellation window of this class has past.</span>
+          <button onClick={event => {
+            console.log('clicked')
+            scheduleComplete(schedule.address, history)
+          }}>Complete class and withdraw ${balance}</button>
         )
       }
-    } else {
-      if (new Date().valueOf() < new Date(schedule.dates.purchase).valueOf()) {
-        return (
-
-          <button type="button" onClick={event => {
-            spotPurchase(schedule, user.address, history, location)
-            }}>
-            {`Buy class for ${eth.web3().fromWei(price)}`}
+    } else if (user.type === UserType.individual || user.type === UserType.reseller) {
+      const price = user.type === UserType.individual ? schedule.price.individual : schedule.price.reseller
+      if (schedule.reserved) {
+        if (new Date().valueOf() < new Date(schedule.dates.cancellation).valueOf()) {
+          return (
+            <button type="button" onClick={event => spotCancel(schedule, user.address, history, location)}>
+              Cancel and refund
           </button>
-        )
+          )
+        } else {
+          return (
+            <span>The cancellation window of this class has past.</span>
+          )
+        }
       } else {
-        return (
-          <span>The purchase window of this class has past.</span>
-        )
+        if (new Date().valueOf() < new Date(schedule.dates.purchase).valueOf()) {
+          return (
+            <div>
+              <Field style={{ float: 'left' }} name="address" component="input" type="text" placeholder="User Address" onChange={event => this.onAddressChanged(event)} />
+              <button type="button" onClick={event => {
+                spotPurchase(schedule, user.address, history, location)
+              }}>
+                {`Buy class for ${eth.web3().fromWei(price)}`}
+              </button>
+            </div>
+          )
+        } else {
+          return (
+            <span>The purchase window of this class has past.</span>
+          )
+        }
       }
     }
   }
 }
 
-export default UserActions
+export default reduxForm({
+  // a unique name for the form
+  form: 'userActions'
+})(UserActions)
+
+//<Field style={{ float: 'left' }} name="address" component="input" type="text" placeholder="User Address" onChange={event => this.onAddressChanged(event)} />
