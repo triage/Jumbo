c3703baab07bc7453d7ae9653330b2968e6544d4
Eric <peoplefromgoodhomes@gmail.com>
2018-03-29 23:34:04 -0400

    wip

--- webpack.common.js
+++ webpack.common.js
@@ -1,19 +1,58 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const combineLoaders = require('webpack-combine-loaders');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
     app: './src/index.js'
   },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.json'],
+    alias: {
+      src: path.resolve(__dirname, 'src/'),
+      user: path.resolve(__dirname, 'src/user/')
+    },
+  },
   plugins: [
-    new CleanWebpackPlugin(['build']),
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Production'
     })
   ],
+  module: {
+    loaders: [
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+      {
+        test: /\.jsx?$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        query: {
+          cacheDirectory: true,
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: combineLoaders([
+          {
+            loader: 'style-loader'
+          }, {
+            loader: 'css-loader',
+            query: {
+              modules: true,
+              localIdentName: '[name]__[local]___[hash:base64:5]'
+            }
+          }
+        ])
+      }
+    ]
+  },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'build')
+    path: path.resolve(__dirname, 'dist')
   }
 };
