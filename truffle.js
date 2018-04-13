4fe7a893ca8279ac271cb6ce4c5f8882c208ca05
Eric <peoplefromgoodhomes@gmail.com>
2018-04-13 15:54:48 +0300

    scripts

--- truffle.js
+++ truffle.js
@@ -1,10 +1,10 @@
 // Allows us to use ES6 in our migrations and tests.
 require('babel-register')
 
-const HDWalletProvider = require("truffle-hdwallet-provider");
+const HDWalletProvider = require('truffle-hdwallet-provider')
 
 module.exports = {
-  migrations_directory: "./migrations",
+  migrations_directory: './migrations',
   // contracts_build_directory: "src/contracts",
   networks: {
     development: {
@@ -12,32 +12,32 @@ module.exports = {
       gas: 6721975,
       gasPrice: 20000000000,
       port: 7545,
-      network_id: "5777" // Match any network id
+      network_id: '5777',
     },
     test: {
       host: 'localhost',
       port: 7545,
       gas: 8007776,
       gasPrice: 2000000000,
-      network_id: "*", // Match any network id
-      provider: () => {
-        return new HDWalletProvider(
+      network_id: '*', // Match any network id
+      provider: () => (
+        new HDWalletProvider(
           'candy maple cake sugar pudding cream honey rich smooth crumble sweet treat',
           'https://ropsten.infura.io/c2M8LGCNQPz9NnAZWkDt'
         )
-      },
+      ),
     },
     ropsten: {
       gas: 4600000,
       // gasPrice: 10000000000, //too long
       gasPrice: 20000000000,
-      provider: () => {
-        return new HDWalletProvider(
+      provider: () => (
+        new HDWalletProvider(
           'lottery hold latin index cool renew scatter cradle begin evil rich cream',
           'https://ropsten.infura.io/c2M8LGCNQPz9NnAZWkDt'
         )
-      },
-      network_id: 3
+      ),
+      network_id: 3,
     },
-  }
-};
+  },
+}
